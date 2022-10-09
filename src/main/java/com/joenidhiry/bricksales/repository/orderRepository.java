b7f9c77b68c26236fa6f16bdfc9ec37a22c42d31
jnidhiry <josephnidhiry@gmail.com>
2022-10-09 23:34:01 +0100

    Initial commit

--- src/main/java/com/joenidhiry/bricksales/repository/orderRepository.java
+++ src/main/java/com/joenidhiry/bricksales/repository/orderRepository.java
@@ -1,8 +1,7 @@
 package com.joenidhiry.bricksales.repository;
 
 import com.joenidhiry.bricksales.model.Order;
-import org.hibernate.cfg.annotations.reflection.internal.JPAXMLOverriddenAnnotationReader;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface orderRepository extends JpaRepository<Order,Integer> {
+public interface OrderRepository extends JpaRepository<Order,Integer> {
 }
