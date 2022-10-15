76e17ec50afc4e198ab53447b75dd772086953d1
jnidhiry <josephnidhiry@gmail.com>
2022-10-15 21:49:35 +0100

    Initial commit

--- src/main/java/com/joenidhiry/bricksales/controller/BrickOrderController.java
+++ src/main/java/com/joenidhiry/bricksales/controller/BrickOrderController.java
@@ -3,6 +3,7 @@ package com.joenidhiry.bricksales.controller;
 import com.joenidhiry.bricksales.model.Order;
 import com.joenidhiry.bricksales.service.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
@@ -16,30 +17,32 @@ public class BrickOrderController {
     private OrderService orderService;
 
     @PostMapping("/submitBrickOrder")
-    public Order addOrder(@RequestBody Order order) {
-        return orderService.createOrder(order);
+    public ResponseEntity<Order> addOrder(@RequestBody Order order) {
+
+        return ResponseEntity.ok(orderService.createOrder(order));
     }
 
     @GetMapping("/BrickOrder/{id}")
-    public Order getOrderByReference(@PathVariable int orderReference) {
+    public ResponseEntity<Order> getOrderByReference(@PathVariable int orderReference) {
         Optional<Order> optionalOrderByRef   = orderService.getOrderByRef(orderReference);
-        return optionalOrderByRef.get();
+        return ResponseEntity.ok(optionalOrderByRef.get());
     }
 
-    @GetMapping("/brickOrders")
-    public List<Order> retrieveAllOrders() {
-        return orderService.getOrders();
+    @GetMapping("/BrickOrders")
+    public ResponseEntity<List<Order>> retrieveAllOrders() {
+
+        return ResponseEntity.ok(orderService.getOrders());
     }
 
     @PostMapping("/DispatchBrickOrder/{orderReference}/{dispatchDate}")
-    public Order dispatchOrder(@PathVariable int orderReference, @PathVariable String dispatchDate) {
+    public ResponseEntity<Order> dispatchOrder(@PathVariable int orderReference, @PathVariable String dispatchDate) {
         Optional<Order> optionalOrderByRef = orderService.getOrderByRef(orderReference);
         String message1;
         if (optionalOrderByRef.isPresent()) {
     /*        return optionalOrderByRef.get().setDispatchDate(orderReference, dispatchDate); */
-              return optionalOrderByRef.get();
+              return ResponseEntity.ok(optionalOrderByRef.get());
         } else {
-            return null;
+            return ResponseEntity.ok(null);
         }
 
     }
