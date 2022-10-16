db0143c62751d76c2b27ac88a36b8e565e424578
jnidhiry <josephnidhiry@gmail.com>
2022-10-16 19:17:15 +0100

    Initial commit

--- src/main/java/com/joenidhiry/bricksales/model/Order.java
+++ src/main/java/com/joenidhiry/bricksales/model/Order.java
@@ -6,6 +6,7 @@ import org.springframework.lang.Nullable;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import java.math.BigDecimal;
 import java.time.LocalDate;
@@ -14,7 +15,7 @@ import java.time.LocalDate;
 @Entity
 public class Order {
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer orderReference;
     private String customerName;
     private BigDecimal quantityOrdered;
