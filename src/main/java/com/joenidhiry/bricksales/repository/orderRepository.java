0ec08c4d3cb60d6cfe2c12647a94a53a4486a50c
jnidhiry <josephnidhiry@gmail.com>
2022-10-17 00:55:28 +0100

    Initial commit

--- src/main/java/com/joenidhiry/bricksales/repository/orderRepository.java
+++ src/main/java/com/joenidhiry/bricksales/repository/orderRepository.java
@@ -2,6 +2,7 @@ package com.joenidhiry.bricksales.repository;
 
 import com.joenidhiry.bricksales.model.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 
-public interface OrderRepository extends JpaRepository<Order,Integer> {
+public interface OrderRepository extends CrudRepository<Order,Integer> {
 }
