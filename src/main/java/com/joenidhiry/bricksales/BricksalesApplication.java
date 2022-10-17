bd6c933d81faf19974effa812f9e02b67c40123c
jnidhiry <josephnidhiry@gmail.com>
2022-10-17 01:00:01 +0100

    Initial commit

--- src/main/java/com/joenidhiry/bricksales/BricksalesApplication.java
+++ src/main/java/com/joenidhiry/bricksales/BricksalesApplication.java
@@ -1,8 +1,15 @@
 package com.joenidhiry.bricksales;
 
+import com.joenidhiry.bricksales.model.Order;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
+@EnableAutoConfiguration
+@EntityScan(basePackageClasses= Order.class)
 @SpringBootApplication
 public class BricksalesApplication {
 
