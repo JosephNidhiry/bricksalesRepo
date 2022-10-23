4539ef32d4ca5a0ad00b51b81af945ff39d5e100
jnidhiry <josephnidhiry@gmail.com>
2022-10-23 22:47:25 +0100

    Initial commit

--- src/main/java/com/joenidhiry/bricksales/BricksalesApplication.java
+++ src/main/java/com/joenidhiry/bricksales/BricksalesApplication.java
@@ -20,7 +20,7 @@ import javax.sql.DataSource;
 import java.math.BigDecimal;
 
 @SpringBootApplication
-@EnableJpaRepositories
+
 public class BricksalesApplication {
 	@Autowired
 	OrderService orderService;
--- src/main/java/com/joenidhiry/bricksales/model/Order.java
+++ src/main/java/com/joenidhiry/bricksales/model/Order.java
@@ -1,15 +1,13 @@
 package com.joenidhiry.bricksales.model;
 import org.springframework.lang.Nullable;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
 
 @Entity
+@Table
 public class Order {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
